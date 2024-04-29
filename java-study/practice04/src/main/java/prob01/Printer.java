8258465800e0797151f050bbf36192e406507445
sohyun-ahn <sohyun5747@gmail.com>
2024-04-29 14:40:54 +0900

    Refactor: Use generics

--- java-study/practice04/src/main/java/prob01/Printer.java
+++ java-study/practice04/src/main/java/prob01/Printer.java
@@ -1,16 +1,46 @@
 package prob01;
 
 public class Printer {
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	public void println(boolean i) {
+//		System.out.println(i);
+//	}
+//	public void println(double i) {
+//		System.out.println(i);
+//	}
+//	public void println(String i) {
+//		System.out.println(i);
+//	}
+	
+	// generic으로 바꾸기
+//	public <T> T println(T t) {
+//		System.out.println(t);
+//		return t;
+//	}
+//	public <T, S> S println(T t, S s) {
+//		System.out.println(t);
+//		return s;
+//	}
+	// 제네릭 실습
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-	public void println(boolean i) {
-		System.out.println(i);
+	
+	// 가변 파라미터 실습
+	public int sum(Integer... nums) {
+		int s = 0;
+		for(Integer n : nums) {
+			s += n;
+		}
+		return s;
 	}
-	public void println(double i) {
-		System.out.println(i);
+	
+	// 가변이면서, 타입도 지정되지않는 경우
+	// 오버로딩 대체하지말고, 
+	public <T> void println(T... t) {
+		System.out.println(t);
 	}
-	public void println(String i) {
-		System.out.println(i);
-	}
+	
 }
--- java-study/practice04/src/main/java/prob01/PrinterTest.java
+++ java-study/practice04/src/main/java/prob01/PrinterTest.java
@@ -4,10 +4,19 @@ public class PrinterTest {
 
 	public static void main(String[] args) {
 		Printer printer = new Printer();
-
+		// printer.<Integer>println( 10 ); // 이게 맞지만, 숨은 기능(파라미터 타입으로 컴파일러가 추론하는 기능)이 있어서 안써도 가능. 
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+
+		// 가변 파라미터 실습
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1,2,3));
+		System.out.println(printer.sum(1,2,3,4,5));
+		
+		printer.println( 10, "홍길동" );
+		printer.println( 10, true, "홍길동" );
 	}
+	
 }
