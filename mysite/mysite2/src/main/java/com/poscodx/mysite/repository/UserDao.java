a58000a73dc6f78f741a514f0da7f4fdfe52f732
sohyun-ahn <sohyun5747@gmail.com>
2024-06-03 14:53:04 +0900

    Refactor: Delete unnecessary parts

--- mysite/mysite2/src/main/java/com/poscodx/mysite/repository/UserDao.java
+++ mysite/mysite2/src/main/java/com/poscodx/mysite/repository/UserDao.java
@@ -7,192 +7,126 @@ import java.sql.SQLException;
 
 import com.poscodx.mysite.vo.UserVo;
 
-
 public class UserDao {
-	
+
 	public int insert(UserVo vo) {
 		int result = 0;
-		
-		try (
-			Connection conn = DBConnection.get();
-			PreparedStatement pstmt1 = conn.prepareStatement("insert into user values(null, ?, ?, password(?), ?, current_date())");
-			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
-		){
-			// binding  
+
+		try (Connection conn = DBConnection.get();
+				PreparedStatement pstmt1 = conn
+						.prepareStatement("insert into user values(null, ?, ?, password(?), ?, current_date())");
+				PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");) {
+			// binding
 			pstmt1.setString(1, vo.getName());
 			pstmt1.setString(2, vo.getEmail());
 			pstmt1.setString(3, vo.getPassword());
 			pstmt1.setString(4, vo.getGender());
 			pstmt1.setString(5, vo.getJoinDate());
-			
-		    result = pstmt1.executeUpdate(); // insert가 잘 수행시 카운트된 숫자 출력
-		    
-		    // select No
-		    ResultSet rs = pstmt2.executeQuery(); 
-		    vo.setNo(rs.next() ? rs.getLong(1) : null);
+
+			result = pstmt1.executeUpdate(); // insert가 잘 수행시 카운트된 숫자 출력
+
+			// select No
+			ResultSet rs = pstmt2.executeQuery();
+			vo.setNo(rs.next() ? rs.getLong(1) : null);
 			rs.close();
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
-		} 
-		
+		}
+
 		return result;
 	}
-	
 
 	public boolean deleteByNoAndPassword(Long no, String password) {
 		boolean result = false;
-		
-		try (
-			Connection conn = DBConnection.get();
-			PreparedStatement pstmt = conn.prepareStatement("delete from user where no = ? and password = ?");
-		){
-		    // 4. binding
+
+		try (Connection conn = DBConnection.get();
+				PreparedStatement pstmt = conn.prepareStatement("delete from user where no = ? and password = ?");) {
+			// 4. binding
 			pstmt.setLong(1, no);
 			pstmt.setString(2, password);
-		    
-		    // 5. SQL 실행
-		    int count = pstmt.executeUpdate(); // insert가 잘 수행시 카운트된 숫자 출력
-			
+
+			// 5. SQL 실행
+			int count = pstmt.executeUpdate(); // insert가 잘 수행시 카운트된 숫자 출력
+
 			// 6. 결과 처리
-		    result = count == 1;
-			
+			result = count == 1;
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
-		} 
-		
+		}
+
 		return result;
-		
+
 	}
-	
-//	public List<UserVo> findAll() {
-// 		List<UserVo> result = new ArrayList<>();
-// 		Connection conn = DBConnection.get();
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//
-//		try {
-//			conn = DBConnection.get();
-//			
-//			//3. Statement 준비
-//			String sql =
-//				"select name, join_date, content" + 
-//			    " from user" + 
-//				" order by reg_date desc";
-//			pstmt = conn.prepareStatement(sql);
-//
-//			//4. binding
-//
-//			//5. SQL 실행
-//			rs = pstmt.executeQuery();
-//
-//			//6. 결과 처리
-//			while(rs.next()) {
-//				Long no = rs.getLong(1);
-//				String name = rs.getString(2);
-//				String regDate = rs.getString(3);
-//				String content = rs.getString(4);
-//
-//				UserVo vo = new UserVo();
-//				vo.setNo(no);
-//				vo.setName(name);
-//
-//				result.add(vo);
-//			}
-//		} catch (SQLException e) {
-//				System.out.println("error:" + e);
-//		} finally {
-//			try {
-//				if(rs != null) {
-//					rs.close();
-//				}
-//
-//				if(pstmt != null) {
-//					pstmt.close();
-//				}
-//
-//				if(conn != null) {
-//					conn.close();
-//				}
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//
-//		return result;
-//	}
 
 	public UserVo findByNoAndPassword(String email, String password) {
 		UserVo result = null;
-		
-		try (
-			Connection conn = DBConnection.get();
-			PreparedStatement pstmt = conn.prepareStatement("select no, name from user where email = ? and password=password(?)");
-		){
+
+		try (Connection conn = DBConnection.get();
+				PreparedStatement pstmt = conn
+						.prepareStatement("select no, name from user where email = ? and password=password(?)");) {
 			pstmt.setString(1, email);
 			pstmt.setString(2, password);
-			
-		    ResultSet rs = pstmt.executeQuery(); 
-		    if(rs.next()) {
-		    	Long no = rs.getLong(1);
-		    	String name = rs.getString(2);
-		    	
-		    	result = new UserVo();
-		    	result.setNo(no);
-		    	result.setName(name);
-		    }
-		    rs.close();
-			    
-		}catch (SQLException e) {
-					System.out.println("error:" + e);
-		} 
-				
+
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+
+				result = new UserVo();
+				result.setNo(no);
+				result.setName(name);
+			}
+			rs.close();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
 		return result;
 	}
-
 
 	public UserVo findByNo(Long no) {
 		UserVo result = null;
-		
-		try (
-			Connection conn = DBConnection.get();
-			PreparedStatement pstmt = conn.prepareStatement("select name, email, password, gender from user where no = ?");
-		){
+
+		try (Connection conn = DBConnection.get();
+				PreparedStatement pstmt = conn
+						.prepareStatement("select name, email, password, gender from user where no = ?");) {
 			pstmt.setLong(1, no);
-			
-		    ResultSet rs = pstmt.executeQuery(); 
-		    if(rs.next()) {
-		    	String name = rs.getString(1);
-		    	String email = rs.getString(2);
-		    	String password = rs.getString(3);
-		    	String gender = rs.getString(4);
-		    	
-		    	result = new UserVo();
-		    	result.setName(name);
-		    	result.setEmail(email);
-		    	result.setPassword(password);
-		    	result.setGender(gender);
-		    }
-		    rs.close();
-			    
-		}catch (SQLException e) {
-					System.out.println("error:" + e);
-		} 
-		
+
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				String name = rs.getString(1);
+				String email = rs.getString(2);
+				String password = rs.getString(3);
+				String gender = rs.getString(4);
+
+				result = new UserVo();
+				result.setName(name);
+				result.setEmail(email);
+				result.setPassword(password);
+				result.setGender(gender);
+			}
+			rs.close();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
 		return result;
 	}
 
-
 	public Integer update(UserVo userVo) {
 		int result = 0;
-		
-		try (
-			Connection conn = DBConnection.get(); 
-			PreparedStatement pstmt = conn.prepareStatement("update user set name = ?, gender = ? where no = ?");
-			PreparedStatement pstmt2 = conn.prepareStatement("update user set name = ?, password = password(?), gender = ? where no = ?");
-			
+
+		try (Connection conn = DBConnection.get();
+				PreparedStatement pstmt = conn.prepareStatement("update user set name = ?, gender = ? where no = ?");
+				PreparedStatement pstmt2 = conn
+						.prepareStatement("update user set name = ?, password = password(?), gender = ? where no = ?");
+
 		) {
-			if("".equals(userVo.getPassword())) {
+			if ("".equals(userVo.getPassword())) {
 				pstmt.setString(1, userVo.getName());
 				pstmt.setString(2, userVo.getGender());
 				pstmt.setLong(3, userVo.getNo());
@@ -204,11 +138,11 @@ public class UserDao {
 				pstmt2.setLong(4, userVo.getNo());
 				result = pstmt2.executeUpdate();
 			}
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		}
-		
+
 		return result;
 	}
 }
--- mysite/mysite2/src/main/webapp/WEB-INF/views/includes/header.jsp
+++ mysite/mysite2/src/main/webapp/WEB-INF/views/includes/header.jsp
@@ -1,7 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jstl/fmt" prefix="fmt" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
-<%@page import="com.poscodx.mysite.vo.UserVo"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <div id="header">
