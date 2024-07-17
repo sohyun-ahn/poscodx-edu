4f7dd999dd387a3533f0d74476340af6ce64c612
sohyun-ahn <sohyun5747@gmail.com>
2024-07-17 15:27:45 +0900

    Feat: Add backend files

--- react-practices/tabbox/frontend/src/TabBox.js
+++ react-practices/tabbox/frontend/src/TabBox.js
@@ -5,14 +5,7 @@ import { tab_box } from "./assets/scss/TabBox.scss";
 import tabs from "./assets/json/data";
 
 function TabBox() {
-  // json/data.js 파일에 넣음
-  // const tabs = [
-  //   { no: 1, name: "메뉴1", active: false, content: "메뉴1 입니다" },
-  //   { no: 2, name: "메뉴2", active: false, content: "메뉴2 입니다" },
-  //   { no: 3, name: "메뉴3", active: true, content: "메뉴3 입니다" },
-  //   { no: 4, name: "메뉴4", active: false, content: "메뉴4 입니다" },
-  //   { no: 5, name: "메뉴5", active: false, content: "메뉴5 입니다" },
-  // ];
+  // tabs 데이터를 json/data.js 파일에 넣음
 
   const [activeNo, setActiveNo] = useState(0);
   const handleSelectedTab = (no) => {
--- react-practices/tabbox/frontend/src/assets/json/data.js
+++ react-practices/tabbox/frontend/src/assets/json/data.js
@@ -1,7 +1,9 @@
 export default [
-  { no: 1, name: "메뉴1", active: false, content: "메뉴1 입니다" },
-  { no: 2, name: "메뉴2", active: false, content: "메뉴2 입니다" },
-  { no: 3, name: "메뉴3", active: false, content: "메뉴3 입니다" },
-  { no: 4, name: "메뉴4", active: false, content: "메뉴4 입니다" },
-  { no: 5, name: "메뉴5", active: false, content: "메뉴5 입니다" },
+  { no: 10, name: "메뉴1", active: false, content: "메뉴1의 뷰 내용" },
+  { no: 20, name: "메뉴2", active: false, content: "메뉴2의 뷰 내용" },
+  { no: 3, name: "메뉴3", active: false, content: "메뉴3의 뷰 내용" },
+  { no: 41, name: "메뉴4", active: false, content: "메뉴4의 뷰 내용" },
+  { no: 5, name: "메뉴5", active: false, content: "메뉴5의 뷰 내용" },
+  { no: 62, name: "메뉴6", active: false, content: "메뉴6의 뷰 내용" },
+  { no: 17, name: "메뉴7", active: false, content: "메뉴7의 뷰 내용" },
 ];
