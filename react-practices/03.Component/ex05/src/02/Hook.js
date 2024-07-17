48c9daf09fb8d1cecafc44ba7ef8739bd89f4364
sohyun-ahn <sohyun5747@gmail.com>
2024-07-17 18:54:42 +0900

    Feat: Study react hooks for state management

--- react-practices/03.Component/ex05/src/02/Hook.js
+++ react-practices/03.Component/ex05/src/02/Hook.js
@@ -1,16 +1,58 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Hook({ color }) {
-    return (
-        <>
-            <h3
-                style={ {
-                    width: 300,
-                    height: 50,
-                    backgroundColor: color
-                } } />
-            <input
-                type='text' />
-        </>
-    );
+  const [backgroundColor, setBackgroundColor] = useState(null);
+  const [text, setText] = useState("");
+
+  /**
+   * 1. Alternative 01: getDerivedStateFromProps
+   */
+  if (color !== backgroundColor) {
+    setBackgroundColor(color);
+  }
+
+  /**
+   * 2. After Rendering
+   * class component(componentDidUpdate, componentDidMount)
+   */
+
+  useEffect(() => {
+    console.log("After Rendering: update text or update background color");
+  });
+
+  /**
+   * 3. After Rendering
+   * 어떤 특정 상태의 변화에 반응하는 함수
+   */
+  useEffect(() => {
+    console.log("After Rendering: update text");
+  }, [text]);
+
+  /**
+   * 4. Alternative 02: componentDidMount & componentWillUnmount
+   */
+
+  useEffect(() => {
+    console.log("After Mount(componentDidMount)");
+    return () => {
+      console.log("After Unmount(componentWillUnmount)");
+    };
+  }, []);
+
+  return (
+    <>
+      <h3
+        style={{
+          width: 300,
+          height: 50,
+          backgroundColor: backgroundColor,
+        }}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </>
+  );
 }
--- react-practices/03.Component/ex05/src/04/App.js
+++ react-practices/03.Component/ex05/src/04/App.js
@@ -1,15 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { useState, useEffect } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default function App() {
-    return(
-        <>
-            <Clock
-                title={`ex05: Clock Component II: 0`}
-                hours={12}
-                minutes={0}
-                seconds={0} />
-        </>
-    );    
+  const [ticks, setTicks] = useState(0);
+
+  const getCurrentTime = function () {
+    const now = new Date();
+    return {
+      hours: now.getHours(),
+      minutes: now.getMinutes(),
+      seconds: now.getSeconds(),
+      tick: this.state ? this.state.tick + 1 : 0,
+    };
+  };
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+      setTicks((x) => x + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <Clock
+        title={`ex05: Clock Component II: 0`}
+        hours={state.hours}
+        minutes={state.minutes}
+        seconds={state.seconds}
+      />
+    </>
+  );
 }
